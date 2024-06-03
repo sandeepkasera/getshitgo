2feefd19a4e640151a36712b96cab60af5f3312e
Sandeep Kumar <sandeepkasera>
2024-06-03 21:14:43 +0530

    Payment Gateway

--- src/app/(pages)/cart/page.tsx
+++ src/app/(pages)/cart/page.tsx
@@ -1,8 +1,11 @@
 "use client"
 import Header from '@/app/components/Header';
-import React from 'react';
+import PaymentCheckout from '@/app/components/paymentCheckout';
+import React, {useState, useEffect} from 'react';
 import productList from '@/app/assets/data/productlist.json'
 import { useCart } from '@/app/context/cart/cartContext';
+import axios from "axios";
+// import { useRouter } from 'next/router';
 
 const CartPage = () => {
   const {
@@ -15,19 +18,45 @@ const CartPage = () => {
   const handleRemove = (id: number) => {
     remove(id)
   }
-  // const cartItems = [2,3]
   const cartItem = productList.filter(product => cart.includes(product.id));
-
-
   const calculateTotal = () => {
     return cartItem.reduce((total, product) => total + product.price, 0)
   };
+
+  const [orderDetails, setOrderDetails] = useState<object>({});
+  const [checkoutVisible, setCheckoutVisible] = useState<boolean>(false);
+  const onCheckout = async (amount: number, id: string) => {
+    const options = {
+      "amount": amount,
+      "currency": "INR",
+      "receipt": id
+    }
+    try {
+      const response = await axios.post("/api/payment/razorpay", options);
+      setOrderDetails(response.data);
+      setCheckoutVisible(!checkoutVisible)
+        
+      // const router = useRouter();
+      // router.push(`/pay?total=${calculateTotal()}`);
+      
+       
+    } catch (error: any) {  
+      console.log("err', error");
+            
+    } finally {
+        console.log("finally");
+        
+    }
+}
+useEffect(()=> {
+  setCheckoutVisible(!checkoutVisible)
+},[orderDetails])
 
   return (
     <>
       <Header />
       <div className="container mx-auto p-4">
-        <h1 className="text-3xl font-bold mb-4">Shopping Cart</h1>
+        <h1 className="text-3xl font-bold mb-4">Shopping Cart{checkoutVisible}</h1>
         <div className="bg-white shadow-md rounded-lg p-6">
           {cartItem.length > 0 ? (
             <>
@@ -48,7 +77,8 @@ const CartPage = () => {
               </ul>
               <div className="text-right mt-6">
                 <p className=" text-xl bg-blue-500 text-white px-6 py-2 rounded mt-4 ">Total:Rs {calculateTotal()}</p>
-                <button className="bg-blue-500 text-white px-6 py-2 rounded mt-4">Checkout</button>
+                <button onClick={()=> onCheckout(calculateTotal(), "12345")} className="bg-blue-500 text-white px-6 py-2 rounded mt-4">Checkout</button>
+                {checkoutVisible && <PaymentCheckout order={orderDetails}/>}
               </div>
             </>
           ) : (
