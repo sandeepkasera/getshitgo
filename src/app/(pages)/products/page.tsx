662f5390af8e59950dd5d4b35c1e3b3df3df286d
Sandeep Kumar <sandeepkasera>
2024-06-22 18:04:45 +0530

    Footer added

--- src/app/(pages)/products/page.tsx
+++ src/app/(pages)/products/page.tsx
@@ -1,11 +1,9 @@
-import Header from "@/app/components/Header";
 import Card from "@/app/components/card";
 import productlist from "@/app/assets/data/productlist.json";
 
 const products = () => {
   return (
     <div>
-      <Header />
       <div className="card-list flex-row flex-wrap space-x-4 object-cover sm:flex md:flex justify-center gap-4 mt-4">
         {productlist.map((product) => (
           <Card
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -18,17 +18,14 @@ const Header = () => {
             {isMenuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
           </button>
         </div>
-        <ul className={`md:flex md:items-center md:space-x-6 ${isMenuOpen ? 'block' : 'hidden'} absolute md:static top-16 left-0 w-full md:w-auto bg-gray-800 md:bg-transparent z-10 md:z-auto transition-all duration-300 ease-in-out`}>
+        <ul className={`md:flex md:items-center md:space-x-6 ${isMenuOpen ? 'block' : 'hidden'} absolute md:relative top-16 md:top-auto left-0 w-full md:w-auto bg-gray-800 md:bg-transparent z-10 md:z-auto transition-all duration-300 ease-in-out`}>
           <li className="p-4 md:p-0 md:mx-2 hover:text-gray-300">
             <Link href="/">Home</Link>
           </li>
           <li className="p-4 md:p-0 md:mx-2 hover:text-gray-300">
             <Link href="/cart">Cart</Link>
           </li>
-          {/* <li className="p-4 md:p-0 md:mx-2 hover:text-gray-300">
-            <Link href="/profile">My Profile</Link>
-          </li> */}
-          <div className="relative md:flex items-center space-x-1 p-4 md:p-0">
+          <div className="p-4 md:p-0 md:mx-2 hover:text-gray-300">
             <form action="#" method="GET" className="flex items-center w-full">
               <input type="text" name="search" placeholder="Search Products" className="px-4 py-2 w-full md:w-auto border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-gray-800" />
               <button type="submit" className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
