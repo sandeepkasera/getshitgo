33e9a696f56f139c5214ab5f6c515e8075e5918c
Sandeep Kumar <sandeepkasera>
2024-06-23 01:08:16 +0530

    remove option from list

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -21,17 +21,19 @@ const Card = ({ id, brand, name, size, price, image }: cardProps) => {
     remove } = useCart();
 
   const handleAddToCart = (id: number) => {
+    setIsVisible(false)
     addTocart(id)
   }
   const handleRemove = (id: number) => {
+    setIsVisible(true)
     remove(id)
   }
   const [isVisible, setIsVisible] = useState<boolean>(true);
   useEffect(() => {
     if (cart.includes(id)) {
       setIsVisible(false)
-    }
-  }, [cart])
+    }    
+  }, [])
   return (
     <div className="bg-white border-white border-2 max-w-sm rounded overflow-hidden shadow-lg flex flex-col m-4">
       <div className="aspect-w-3 aspect-h-4">
@@ -73,6 +75,23 @@ const Card = ({ id, brand, name, size, price, image }: cardProps) => {
               <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
             </svg>
             Add to cart
+          </button>
+        </div>
+      )}
+      {!isVisible && (
+        <div className="flex justify-center mb-2">
+          <button className="flex items-center justify-center bg-gray-900 px-2 py-1  text-sm text-white transition hover:bg-gray-700"
+            onClick={() => handleRemove(id)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="mr-2 h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
+            </svg>
+            Remove from Cart
           </button>
         </div>
       )}
